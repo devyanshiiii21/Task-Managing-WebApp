3a2fa201aec3ba0a325a9f15e4abec4bcaa49113
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-04-18 21:50:41 +0530

    Register form

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete'),
     path('login/',CustomLoginView.as_view(), name='login' ),
     path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'), 
 
 ]
